4e8344250f5b36123ed4e04cfaee2daba5466083
Максим Михалёв <mihalevm0104@gmail.com>
2025-08-29 17:07:33 +0300

    Update Task3.py

--- Homework_6/Task3.py
+++ Homework_6/Task3.py
@@ -1,3 +1,3 @@
-сelsius = int(input("Введите температуру в градусах Цельсия: "))
+сelsius = float(input("Введите температуру в градусах Цельсия: "))
 fahrenheit = сelsius * 9 / 5 + 32
 print(f"{сelsius}°C это {fahrenheit}°F")
